479093fff0642135cdf6b5e7519c902691dcf494
Gopal Gupta <gopalgupta7799@gmail.com>
2022-06-27 23:04:45 +0530

    Added Hovering and Active Link Effects and Tooltip in NavLinks

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import './Navbar.css'
-import logo from '../../Images/logo.jpg'
+import logo from '../../Images/logo.png'
 import { NavLink } from 'react-router-dom'
 // import Home from '@mui/icons-material'
 // import { AiOutlineHome } from 'react-icons/ai'
@@ -13,12 +13,22 @@ export default function Navbar() {
   return (
     <div id='navbar'>
       <img src={logo} alt="Gopal" id='logo' />
-      <ul id='navList'>
-        <li><NavLink to='/' className="navListItem" id="listItem-1" activeClassName='active'><Home sx={{ color: 'orange' }} /></NavLink></li>
-        <li><NavLink to='/about' className="navListItem" id="listItem-2" ><InfoIcon sx={{ color: '#2eb1b1' }} /></NavLink></li>
-        <li><NavLink to='/projects' className="navListItem" id="listItem-3" ><ListAltIcon sx={{ color: '#2f2dc9' }} /></NavLink></li>
-        <li><NavLink to='/contact' className="navListItem" id="listItem-4" ><ConnectWithoutContactIcon sx={{ color: 'green' }} /></NavLink></li>
-      </ul>
+      <NavLink to='/' className="navListItem" id="listItem-1" activeClassName='active'>
+        <Home className='icon' sx={{ color: 'orange' }} />
+        <span className='tooltipText'>Home</span>
+      </NavLink>
+      <NavLink to='/about' className="navListItem" id="listItem-2" >
+        <InfoIcon className='icon' sx={{ color: '#2eb1b1' }} />
+        <span className='tooltipText'>About</span>
+      </NavLink>
+      <NavLink to='/projects' className="navListItem" id="listItem-3" >
+        <ListAltIcon className='icon' sx={{ color: '#2f2dc9' }} />
+        <span className='tooltipText'>Projects</span>
+      </NavLink>
+      <NavLink to='/contact' className="navListItem" id="listItem-4" >
+        <ConnectWithoutContactIcon className='icon' sx={{ color: 'green' }} />
+        <span className='tooltipText'>Contact</span>
+      </NavLink>
     </div >
   )
 }
